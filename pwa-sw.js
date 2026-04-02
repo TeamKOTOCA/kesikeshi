1005b138a3a895e374fe76d27439def8aa928452
GOTOUMA-KAKERU <43anillo@gmail.com>
2026-04-02 17:43:59 +0900

    a

---
+++ pwa-sw.js
@@ -0,0 +1,57 @@
+﻿const CACHE_NAME = 'kesikeshi-shell-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './worker.js',
+  './manifest.webmanifest',
+  './public/icon-192.png',
+  './public/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((oldKey) => caches.delete(oldKey))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type === 'opaque') {
+            return networkResponse;
+          }
+
+          const clone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return networkResponse;
+        })
+        .catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+          return caches.match('./public/icon-192.png');
+        });
+    })
+  );
+});
